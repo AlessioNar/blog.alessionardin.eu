4427fecb1ba01e837c97a541b85f56997a9982ab
Alessio Nardin <alessio.nardin@gmail.com>
2022-05-13 14:28:19 +0200

    Reinstated apps

--- blogalessionardineu/settings/base.py
+++ blogalessionardineu/settings/base.py
@@ -26,12 +26,12 @@ BASE_DIR = os.path.dirname(PROJECT_DIR)
 INSTALLED_APPS = [
     'home',
     'search',
-    #'flex',
-    #'streams',
-    #'site_settings',
-    #'subscribers',
-    #'blog',
-    #'menus',
+    'flex',
+    'streams',
+    'site_settings',
+    'subscribers',
+    'blog',
+    'menus',
 
     'wagtail.contrib.forms',
     'wagtail.contrib.redirects',
